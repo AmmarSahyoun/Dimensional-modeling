8ab8637b6947ff1aed5ba598dfb0b729c011366f
Ammar sahyoun <ammarzgr@hotmail.com>
2023-08-02 16:53:43 +0200

    add check queries

--- src/DQL/BiggestOrders.sql
+++ src/DQL/BiggestOrders.sql
@@ -1,5 +1,9 @@
- SELECT s.SalesOrderId, c.EmailAddress, p.ProductName, s.Quantity
- FROM sales_fct AS s
- JOIN customer_dim AS c ON s.CustomerId = c.CustomerId
- JOIN product_dim AS p ON s.ProductId = p.ProductId
- ORDER BY Quantity desc
+
+SELECT s.SalesOrderId, c.EmailAddress, p.ProductName, s.Quantity
+FROM sales_fct AS s
+    JOIN customer_dim AS c ON s.CustomerId = c.CustomerId
+    JOIN product_dim AS p ON s.ProductId = p.ProductId
+ORDER BY Quantity desc
+
+
+
