b707bbac6adbe3b107b4b57c87d64146fc8e53f5
Ammar sahyoun <ammarzgr@hotmail.com>
2023-08-02 11:31:50 +0200

    customers query

---
+++ src/DQL/BiggestOrders.sql
@@ -0,0 +1,5 @@
+ SELECT s.SalesOrderId, c.EmailAddress, p.ProductName, s.Quantity
+ FROM sales_fct AS s
+ JOIN customer_dim AS c ON s.CustomerId = c.CustomerId
+ JOIN product_dim AS p ON s.ProductId = p.ProductId
+ ORDER BY Quantity desc
